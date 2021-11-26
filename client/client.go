42526ba0f6c04163a21ffc49160f9ac335fed23b
Aleksei Chernevskii <aleksei_chernevskii@epam.com>
2021-11-26 18:43:18 +0300

    Created client package which will aggregate different B1DDI API clients
    * For now b1ddi.Client supports IPAM and DNS Configuration API clients.

---
+++ client/client.go
@@ -0,0 +1,27 @@
+package client
+
+import (
+	"github.com/go-openapi/runtime"
+	"github.com/go-openapi/strfmt"
+	"github.com/infobloxopen/b1ddi-go-client/dns_config"
+	"github.com/infobloxopen/b1ddi-go-client/ipamsvc"
+)
+
+type Client struct {
+	IPAddressManagementAPI *ipamsvc.IPAddressManagementAPI
+	DNSConfigurationAPI    *dns_config.DNSConfigurationAPI
+}
+
+func NewClient(transport runtime.ClientTransport, formats strfmt.Registry) *Client {
+	return &Client{
+		IPAddressManagementAPI: ipamsvc.New(transport, formats),
+		DNSConfigurationAPI:    dns_config.New(transport, formats),
+	}
+}
+
+// B1DDIToken provides a header for the BloxOne DDI token authentication
+func B1DDIToken(token string) runtime.ClientAuthInfoWriter {
+	return runtime.ClientAuthInfoWriterFunc(func(r runtime.ClientRequest, _ strfmt.Registry) error {
+		return r.SetHeaderParam("Authorization", "Token "+token)
+	})
+}
