00a1b2b574189e4c14a9bf78562b336988ff6168
Aleksei Chernevskii <aleksei_chernevskii@epam.com>
2022-01-20 12:43:08 +0300

    Generated the go client for B1DDI DNS Data API
    * Now one can create DNS Record objects from the DNS Data API using the
      generated b1ddi-go-client.

--- client/client.go
+++ client/client.go
@@ -4,18 +4,21 @@ import (
 	"github.com/go-openapi/runtime"
 	"github.com/go-openapi/strfmt"
 	"github.com/infobloxopen/b1ddi-go-client/dns_config"
+	"github.com/infobloxopen/b1ddi-go-client/dns_data"
 	"github.com/infobloxopen/b1ddi-go-client/ipamsvc"
 )
 
 type Client struct {
 	IPAddressManagementAPI *ipamsvc.IPAddressManagementAPI
 	DNSConfigurationAPI    *dns_config.DNSConfigurationAPI
+	DNSDataAPI             *dns_data.DNSDataAPI
 }
 
 func NewClient(transport runtime.ClientTransport, formats strfmt.Registry) *Client {
 	return &Client{
 		IPAddressManagementAPI: ipamsvc.New(transport, formats),
 		DNSConfigurationAPI:    dns_config.New(transport, formats),
+		DNSDataAPI:             dns_data.New(transport, formats),
 	}
 }
 
